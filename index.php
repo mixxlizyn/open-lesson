100fb616cc362ce3151821d0adab91f02128329f
mixxlizyn <148847723+mixxlizyn@users.noreply.github.com>
2024-03-05 11:02:43 +0500

    Лиза

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+$userEmail = isset($_POST['email']) ? $_POST['email'] : false;
+if (preg_match("/[a-z]{1,}+@[a-z]/", $userEmail)) {
+    "<script>email введена верно.</script>";
+} else {
+    "<script>неправильно введена почта.</script>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <footer>
+        <div class="footer-colomn1">
+            <img src="images\footer-logo.svg" alt="логотип компании" class="footer-logo">
+            <p>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit. Id risus vulputate.</p>
+        </div>
+        <div class="footer-colomn2">
+            <ul>
+                <li>
+                    <h2>contact us</h2>
+                </li>
+                <li><a href="">Mailing address</a></li>
+                <li><a href="">Suite 1100</a></li>
+                <li><a href="">0197876236</a></li>
+                <li><a href="">gmail</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>Company</h2>
+                </li>
+                <li><a href="">Crypto widgets</a></li>
+                <li> <a href="">Crypto currency</a></li>
+                <li><a href="">Bitcoin guid</a></li>
+                <li><a href="">contact us</a></li>
+                <li><a href="">service</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>Navigate</h2>
+                </li>
+                <li><a href="">Roadmap</a></li>
+                <li><a href="">video</a></li>
+                <li><a href="">Team</a></li>
+                <li><a href="">C oin disttibution</a></li>
+                <li><a href="">Media</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>products</h2>
+                </li>
+                <li><a href="">Hardware</a></li>
+                <li><a href="">Mining</a></li>
+                <li><a href="">Terade calls</a></li>
+                <li><a href="">Software</a></li>
+                <li><a href="">media</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-privacy">
+
+            <p class="copy">Copyright © 2021 Brandoxide. All rights <br> reserved.
+            </p>
+            <div class="footer-privacy-p">
+                <p>Terms & Agreements</p>
+                <p>Privecy Policy</p>
+            </div>
+
+        </div>
+    </footer>
+</body>
+
+</html>
